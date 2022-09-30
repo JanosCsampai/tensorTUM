3e000338dfd9065b2ab7d632569f325d63d5c403
akshat <akshat.tandon@tum.de>
2022-09-30 14:53:08 +0200

    structuring the frontend

---
+++ frontend/src/controller/Controller.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+class Controller {
+
+    // add the api calls
+    // TODO (@Get mapping) for getting the images from cloud
+    // TODO: REST calls for verifying answer with the ML model
+
+
+}
+
+export default new Controller();
